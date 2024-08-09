5f7a48daea3ab49e1d612bae71a5f073766194a2
j.rogercorreamello <j.rogercorreamello@gmail.com>
2024-08-09 22:39:56 +0000

    Aula 01

--- aula01/src/Main.kt
+++ aula01/src/Main.kt
@@ -1,17 +1,22 @@
 fun main() {
-    println("CALCULADORA DE JUROS COMPOSTOS")
-    println("------------------------------")
+    val red = "\u001B[31m"
+    val green = "\u001B[32m"
+    val yellow = "\u001B[33m"
+    val reset = "\u001B[0m"
 
-    print("Informe o valor inicial do investimento: R$")
+    println("${red}CALCULADORA DE JUROS COMPOSTOS")
+    println("------------------------------${reset}")
+
+    print("${yellow}Informe o valor inicial do investimento: ${green}R$")
     val valorInicial = readLine()?.toDoubleOrNull() ?: 0.0
 
-    print("Informe a taxa de juros mensal (em %): ")
+    print("${yellow}Informe a taxa de juros mensal (em %):${reset} ")
     val taxaJuros = readLine()?.toDoubleOrNull() ?: 0.0
 
-    print("Informe o número de meses: ")
+    print("${yellow}Informe o número de meses:${reset} ")
     val numeroMeses = readLine()?.toIntOrNull() ?: 0
 
     val montanteFinal = valorInicial * Math.pow(1 + (taxaJuros / 100), numeroMeses.toDouble())
 
-    println("O valor final do investimento será: R$ %.2f".format(montanteFinal))
+    println("O valor final do investimento será: ${green}R$ %.2f".format(montanteFinal))
 }
